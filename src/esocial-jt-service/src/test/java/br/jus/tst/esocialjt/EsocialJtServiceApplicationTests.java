6833212a341ebef442a39ac82c8e2c9f0b43c3e3
Tiago Bento <tiago.bento@tst.jus.br>
2021-08-20 15:25:59 -0300

    Introduz erro proposital para teste de build

--- src/esocial-jt-service/src/test/java/br/jus/tst/esocialjt/EsocialJtServiceApplicationTests.java
+++ src/esocial-jt-service/src/test/java/br/jus/tst/esocialjt/EsocialJtServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package br.jus.tst.esocialjt;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class EsocialJtServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		fail();
 	}
 
 }
