e3d0c9e6f1d81c562bb3b9cb1517356904b5a4f8
Tiago Bento <tiago.bento@tst.jus.br>
2023-09-05 19:15:44 -0300

    Facilita a exclusão de eventos

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
+import ScrollToTop from "../components/scroll/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes />
     </Router>
   );
---
+++ src/esocial-jt-dominio/src/main/java/br/jus/tst/esocial/ocorrencia/dados/DadosFechamentoFolha.java
@@ -0,0 +1,7 @@
+package br.jus.tst.esocial.ocorrencia.dados;
+
+import br.jus.tst.esocial.dominio.ideEvento.IdeEventoFolhaSemRetificacao;
+
+public interface DadosFechamentoFolha {
+    IdeEventoFolhaSemRetificacao getIdeEvento();
+}
