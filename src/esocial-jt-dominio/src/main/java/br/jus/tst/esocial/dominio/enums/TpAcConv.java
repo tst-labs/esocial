98ae0f0cc21d3a15cd559bf1e54a31f844c980f4
Tiago Bento <tiago.bento@tst.jus.br>
2024-11-08 17:55:34 -0300

    Adiciona novos tipos ao enum TpAcConv

--- src/esocial-jt-dominio/src/main/java/br/jus/tst/esocial/dominio/enums/TpAcConv.java
+++ src/esocial-jt-dominio/src/main/java/br/jus/tst/esocial/dominio/enums/TpAcConv.java
@@ -50,7 +50,19 @@ public enum TpAcConv {
 	 * periódicos
 	 * 
 	 */
-	H;
+	H,
+
+	/**
+	 * Sentença judicial (exceto reclamatória trabalhista)
+	 *
+	 */
+	I,
+
+	/**
+	 * Parcelas complementares conhecidas após o fechamento da folha
+	 *
+	 */
+	J;
 
 	public String value() {
 		return name();
